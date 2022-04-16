88ad269d274e7cdff7bb40b380f1656a3a72afc6
czl1378 <czl1378@126.com>
2022-04-16 15:53:47 +0800

    Added token holder function

---
+++ netlify/functions/tokenHolder.ts
@@ -0,0 +1,36 @@
+import { Handler } from '@netlify/functions';
+
+import { Pool } from 'pg';
+
+const matchExplorer = /(.*):(.*)@(.*)\/(.*)/.exec(process.env.EXPLORER_PG_URL);
+
+const explorerPGPool = new Pool({
+  host: matchExplorer[3],
+  user: matchExplorer[1],
+  password: matchExplorer[2],
+  database: matchExplorer[4],
+  port: 5432,
+  max: 20
+});
+
+const handler: Handler = async (event) => {
+
+  const { token, timestamp = 0, limit = 100 } = event.queryStringParameters;
+  
+  const res = await explorerPGPool.query(`
+    SELECT * FROM action_receipt_actions 
+    WHERE receipt_receiver_account_id='${token}'
+    AND receipt_included_in_block_timestamp>${timestamp}
+    AND args->>'method_name'='storage_deposit'
+    ORDER BY receipt_included_in_block_timestamp ASC
+    LIMIT ${limit}
+  `);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(res),
+  };
+
+}
+
+export { handler }
