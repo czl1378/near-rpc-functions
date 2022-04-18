720165d6a4b25e91ab6ada816cfe6bfa6444d25a
seesea <seesea@seeseas-iMac.local>
2022-04-18 11:05:18 +0800

    Minor update

--- netlify/functions/dailyTx.js
+++ netlify/functions/dailyTx.js
@@ -1,8 +1,9 @@
 const { Pool } = require('pg');
 
 const matchAnalytics = /(.*):(.*)@(.*)\/(.*)/.exec(process.env.ANALYTICS_PG_URL);
+const matchExplorer = /(.*):(.*)@(.*)\/(.*)/.exec(process.env.EXPLORER_PG_URL);
 
-const pgPool = new Pool({
+const analyticsPGPool = new Pool({
   host: matchAnalytics[3],
   user: matchAnalytics[1],
   password: matchAnalytics[2],
@@ -11,22 +12,40 @@ const pgPool = new Pool({
   max: 20
 });
 
+const explorerPGPool = new Pool({
+  host: matchExplorer[3],
+  user: matchExplorer[1],
+  password: matchExplorer[2],
+  database: matchExplorer[4],
+  port: 5432,
+  max: 20
+});
+
 exports.handler = async (event) => {
 
-
-  console.log('hehe', process.env.ANALYTICS_PG_URL, matchAnalytics);
-
-  const { rows } = await pgPool.query(`
-    SELECT collected_for_day AS date,
-      transactions_count AS total
-    FROM daily_transactions_count
-    WHERE collected_for_day >= DATE_TRUNC('day', NOW() - INTERVAL '2 week')
-    ORDER BY date
-  `);
-
+  const [{ rows: dailyTx }, { rows: recentTxCount }] = await Promise.all([
+    analyticsPGPool.query(`
+      SELECT collected_for_day AS date,
+        transactions_count AS total
+      FROM daily_transactions_count
+      WHERE collected_for_day >= DATE_TRUNC('day', NOW() - INTERVAL '2 week')
+      ORDER BY date
+    `),
+    explorerPGPool.query(`
+      SELECT
+        COUNT(transaction_hash) AS total
+      FROM transactions
+      WHERE
+        block_timestamp > (CAST(EXTRACT(EPOCH FROM NOW() - INTERVAL '1 day') AS bigint) * 1000 * 1000 * 1000)
+    `)
+  ]);
+ 
   return {
     statusCode: 200,
-    body: JSON.stringify(rows),
+    body: JSON.stringify({
+      dailyTx,
+      recentTxCount
+    }),
   };
 
 }
