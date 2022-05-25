6f7577ae3ac372966e519452edbe17a23f5d9ce5
seesea <seesea@seeseas-iMac.local>
2022-05-25 10:25:10 +0800

    Minor update

--- netlify/functions/queryNearscan.js
+++ netlify/functions/queryNearscan.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 
 const match = /(.*):(.*)@(.*)\/(.*)/.exec(process.env.NEARSCAN_PG_URL);
+const secret = process.env.NEARSCAN_SECRET || '';
 
 const pgPool = new Pool({
   host: match[3],
@@ -13,7 +14,14 @@ const pgPool = new Pool({
 
 exports.handler = async (event) => {
 
-  const { query } = JSON.parse(event.body);
+  const { query, secret: requestSecret } = JSON.parse(event.body);
+
+  if (requestSecret !== secret) {
+    return {
+      statuCode: 403,
+      body: 'Forbidden'
+    }
+  }
 
   const { rows } = await pgPool.query(query);
 
