1aa9b3b8839a22ad67356c81fdf9f1d290220c2e
czl1378 <czl1378@126.com>
2022-06-05 21:37:32 +0800

    Minor update

---
+++ netlify/functions/oauth.js
@@ -0,0 +1,25 @@
+const { OAuth2Client } = require('google-auth-library');
+
+exports.handler = async (event) => {
+  const { appchain } = event.queryStringParameters;
+
+  const oauth2Client = new OAuth2Client({
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    redirectUri: `https://${process.env.NETWORK_ID}.oct.network/appchains/${appchain}`
+  });
+
+  const url = oauth2Client.generateAuthUrl({
+    scope: [
+      'https://www.googleapis.com/auth/compute.readonly', 
+      'https://www.googleapis.com/auth/cloud-platform.read-only',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'openid'
+    ]
+  });
+  
+  return {
+    statusCode: 200,
+    body: url,
+  }
+}
